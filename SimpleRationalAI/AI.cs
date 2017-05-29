27b437e6b2b94cb2d04289e83dcfda1364136e9f
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2017-05-29 22:33:34 +0200

    Refactored by extracting methods and removed the delimiter variable in the process.

--- SimpleRationalAI/AI.cs
+++ SimpleRationalAI/AI.cs
@@ -17,82 +17,99 @@ namespace SimpleRationalAI
 
         public void HandleInput(string userInput)
         {
-            char delimiter = ' ';
-            int wordCount = userInput.Split(delimiter).Length;
+            int wordCount = userInput.Split(' ').Length;
 
-            if (wordCount == 1)
+            if (wordCount < 2 || wordCount > 4)
                 Console.WriteLine("Invalid syntax.");
-            else if (userInput.Split(delimiter)[0] == "create" && wordCount == 2) // Create object command
+            else if (userInput.Split(' ')[0] == "create" && wordCount == 2) // Create object command
             {
-                try
-                {
-                    string objectName = userInput.Split(delimiter)[1];
-                    dataStorage.CreateObject(objectName);
-                    Console.WriteLine("An object called {0} has been created", objectName);
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("The object you try to create already exists.");
-                }
-
+                CreateObject(userInput);
             }
-            else if (userInput.Split(delimiter)[1] == "is" && wordCount == 3) // Add type to object command
+            else if (userInput.Split(' ')[1] == "is" && wordCount == 3) // Add type to object command
             {
-                string inputObject = userInput.Split(delimiter)[0];
-                string inputType = userInput.Split(delimiter)[2];
-                UserdefinedObject o = dataStorage.GetObject(inputObject);
-                if (o != null)
-                {
-                    UserdefinedType typeCheck = dataStorage.GetType(o, inputType);
-                    if (typeCheck == null)
-                    {
-                        dataStorage.AddType(ref o, inputType);
-                        Console.WriteLine("The type called {0} has been added to the object called {1}.", inputType, inputObject);
-                    }
-                    else
-                    {
-                        Console.WriteLine("The type you try to add to object {0} has already been added.", inputObject);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("The object you try to define doesn't exist.");
-                }
+                AddTypeToObject(userInput);
             }
-            else if (userInput.Split(delimiter)[1] == "can" && wordCount == 3) // Add verb to object command
+            else if (userInput.Split(' ')[1] == "can" && wordCount == 3) // Add verb to object command
             {
-                string inputObject = userInput.Split(delimiter)[0];
-                string inputVerb = userInput.Split(delimiter)[2];
-                UserdefinedObject o = dataStorage.GetObject(inputObject);
-                if (o != null)
-                {
-                    UserdefinedVerb verbCheck = dataStorage.GetVerb(o, inputVerb);
-                    if (verbCheck == null)
-                    {
-                        dataStorage.AddVerb(ref o, inputVerb);
-                        Console.WriteLine("The verb called {0} has been added to the object called {1}.", inputVerb, inputObject);
-                    }
-                    else
-                    {
-                        Console.WriteLine("The verb you try to add to object {0} has already been added.", inputObject);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("The object you try to define doesn't exist.");
-                }
+                AddVerbToObject(userInput);
             }
-            else if (userInput.Split(delimiter)[0] == "all" && userInput.Split(delimiter)[2] == "can" && wordCount == 4) // Add verb to type command
+            else if (userInput.Split(' ')[0] == "all" && userInput.Split(' ')[2] == "can" && wordCount == 4) // Add verb to type command
             {
-                string inputType = userInput.Split(delimiter)[1];
-                string inputVerb = userInput.Split(delimiter)[3];
-                dataStorage.AddVerbToType(inputVerb, inputType);
+                AddVerbToType(userInput);
             }
             else
             {
                 Console.WriteLine("Syntax invalid.");
             }
-            
+        }
+
+        private void CreateObject(string userInput)
+        {
+            try
+            {
+                string objectName = userInput.Split(' ')[1];
+                dataStorage.CreateObject(objectName);
+                Console.WriteLine("An object called {0} has been created", objectName);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The object you try to create already exists.");
+            }
+        }
+
+        private void AddTypeToObject(string userInput)
+        {
+            string inputObject = userInput.Split(' ')[0];
+            string inputType = userInput.Split(' ')[2];
+            UserdefinedObject o = dataStorage.GetObject(inputObject);
+            if (o != null)
+            {
+                UserdefinedType typeCheck = dataStorage.GetType(o, inputType);
+                if (typeCheck == null)
+                {
+                    dataStorage.AddType(ref o, inputType);
+                    Console.WriteLine("The type called {0} has been added to the object called {1}.", inputType, inputObject);
+                }
+                else
+                {
+                    Console.WriteLine("The type you try to add to object {0} has already been added.", inputObject);
+                }
+            }
+            else
+            {
+                Console.WriteLine("The object you try to define doesn't exist.");
+            }
+        }
+
+        private void AddVerbToObject(string userInput)
+        {
+            string inputObject = userInput.Split(' ')[0];
+            string inputVerb = userInput.Split(' ')[2];
+            UserdefinedObject o = dataStorage.GetObject(inputObject);
+            if (o != null)
+            {
+                UserdefinedVerb verbCheck = dataStorage.GetVerb(o, inputVerb);
+                if (verbCheck == null)
+                {
+                    dataStorage.AddVerb(ref o, inputVerb);
+                    Console.WriteLine("The verb called {0} has been added to the object called {1}.", inputVerb, inputObject);
+                }
+                else
+                {
+                    Console.WriteLine("The verb you try to add to object {0} has already been added.", inputObject);
+                }
+            }
+            else
+            {
+                Console.WriteLine("The object you try to define doesn't exist.");
+            }
+        }
+
+        private void AddVerbToType(string userInput)
+        {
+            string inputType = userInput.Split(' ')[1];
+            string inputVerb = userInput.Split(' ')[3];
+            dataStorage.AddVerbToType(inputVerb, inputType);
         }
     }
 }
